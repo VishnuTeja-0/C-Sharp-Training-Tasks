b8d4f2cf118594451497cde1bf173ff0829ac3e2
Vishnu Teja Chikkala <vishnu.teja.c99@gmail.com>
2021-02-16 19:28:57 +0530

    First Bank Management App Commit

---
+++ C# Task 2/Bank Management System/BankManagementSystem/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankManagement.Models
+{
+    public class BankAccount
+    {
+        private int _id { get; set; }
+        private string _username { get; set; }
+        private string _password { get; set; }
+        private int _balance { get; set; }
+
+    }
+}
